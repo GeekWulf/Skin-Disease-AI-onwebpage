8fe21e5d94e48eeb36fa511a5882bc7dd4db1dfb
GeekWulf <thailand49y@gmail.com>
2024-01-25 22:34:08 +0700

    clened code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-// More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-      
-// the link to your model provided by Teachable Machine export panel
 const URL = "https://teachablemachine.withgoogle.com/models/By6kxe40C/";
       
 let model, webcam, labelContainer, maxPredictions, startButton;
 
-      
-// Load the image model and setup the webcam
 startButton = document.getElementById("buttonStart");
 
 async function init() {
@@ -15,23 +9,15 @@ async function init() {
     const metadataURL = URL + "metadata.json";
     const preWebcam = document.getElementById("pre-webcam");
       
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
-      
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(500, 500, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
+
+    const flip = true; 
+    webcam = new tmImage.Webcam(500, 500, flip); 
+    await webcam.setup(); 
     await webcam.play();
     window.requestAnimationFrame(loop);
-      
-    // append elements to the DOM
     document.getElementById("webcam-container").replaceChild(webcam.canvas, preWebcam);
-    //labelContainer = document.querySelector(".dyn-dect-bar");
     detectBar = document.getElementById("dectectedBar");
     undetectBar = document.getElementById("undectectedBar");
     dyndetectBar = document.querySelector(".dyn-dect-bar");
@@ -39,14 +25,12 @@ async function init() {
 }
 
 async function loop() {
-    webcam.update(); // update the webcam frame
+    webcam.update();
     await predict();
     window.requestAnimationFrame(loop);
 }
       
-// run the webcam image through the image model
 async function predict() {
-    // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
     
     const DetectclassPrediction = prediction[0].probability.toFixed(2);
